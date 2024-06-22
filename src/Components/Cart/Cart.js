afcc15090b44d4352248b30d42d225bb96c333e1
Flaviu <98925455+Flaviu8@users.noreply.github.com>
2024-06-22 01:54:01 +0100

    Update Cart.js

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -16,20 +16,19 @@ function Cart() {
     }
      {
 
+      if(showCart) {
      return (
-           <>
-                 {showCart ? 
-                             <div className='cart-container'>
-                                  <div className='cart-header'>
-                                    <button onClick={HandleClickCartBtn  }><img src={Icon} alt="icon"></img></button>
-                                     <h2>Cart</h2>
-                                  </div>
-                                        <p>Cart is empty!</p>
-                                </div> : ""
+           
+           <div className='cart-container'>
+                <div className='cart-header'>
+                  <button onClick={HandleClickCartBtn  }><img src={Icon} alt="icon"></img></button>
+                   <h2>Cart</h2>
+                </div>
+                      <p>Cart is empty!</p>
+              </div>
                  
-                 }
-            </>
-    
+      
+      }
 )} 
 } 
 
