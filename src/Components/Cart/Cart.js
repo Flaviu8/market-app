ba7b5ae35cf9bd9a30cd8493c63ca76b49f4f5fd
Flaviu <98925455+Flaviu8@users.noreply.github.com>
2024-06-22 10:27:09 +0100

    Update Cart.js

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,17 +1,16 @@
 import { useEffect, useState} from 'react'
 import './Cart.css'
 import  Icon  from '../Cart/right-arrow.png'
-import Main from '../main/Main'
 
 
 
-function Cart({show}) {
+function Cart({handleClickBtn}) {
 
      return (
            
            <div className='cart-container'>
                 <div className='cart-header'>
-                  <button ><img src={Icon} alt="icon"></img></button>
+                  <button onClick={handleClickBtn}><img src={Icon} alt="icon"></img></button>
                    <h2>Cart</h2>
                 </div>
                       <p>Cart is empty!</p>
